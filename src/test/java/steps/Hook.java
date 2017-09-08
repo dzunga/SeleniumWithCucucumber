ee83ce40984acbd59eafd5ee3a827ee26c5afc33
dzunga <dzungle1@yahoo.com>
2017-09-08 23:25:52 +0800

    update chromerdriver
    add \\ to cover chromedriver, syntax error

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -29,7 +29,7 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "D:\\libs\\chromedriver_win32chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\libs\\chromedriver_win32\\chromedriver.exe");
         base.Driver = new ChromeDriver();
     }
 
