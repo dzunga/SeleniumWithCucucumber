758de0199edfe207acd582bbd94262057d7ca34d
dzunga <dzungle1@yahoo.com>
2017-09-08 23:20:44 +0800

    update drivers geodeck and chromer
    update geodeck and chromedrive

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -24,12 +24,12 @@ public class Hook extends BaseUtil{
 
         System.out.println("Opening the browser : Firefox");
 
-        /*System.setProperty("webdriver.firefox.marionette", "D:\\Libs\\geckodriver.exe");
+        /*System.setProperty("webdriver.firefox.marionette", "D:\\libs\\geckodriver-v0.18.0-win32\\geckodriver.exe");
         base.Driver = new FirefoxDriver();*/
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\libs\\chromedriver_win32chromedriver.exe");
         base.Driver = new ChromeDriver();
     }
 
